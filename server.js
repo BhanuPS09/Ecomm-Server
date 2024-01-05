1c4c1a143ddc44c83e17ae0adcc16d01ff176439
Bhanu Pratap Singh <bhanusinghpcm09@gmail.com>
2024-01-05 13:17:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: products_details,
     mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/failure",
+    success_url: "https://ecommerce-bhanups09.netlify.app/success",
+    cancel_url: "https://ecommerce-bhanups09.netlify.app/failure",
   });
 
   res.json({ id: session.id });
